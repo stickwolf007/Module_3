96449b0a7beb2f531c80f99ca1c6086880ca9752
Олег <oleg_stick@mail.ru>
2024-08-20 14:19:24 +0300

    new

--- module_3_1.py
+++ module_3_1.py
@@ -18,13 +18,12 @@ def is_contains(string, list_to_search):
     for i in list_to_search:
         if i.lower() == string.lower():
             return True
-    else:
-        return False
+    return False
 
 
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('cycle', ['recycling', 'cyclic', 'cycle'])) # No matches
 print(calls)
